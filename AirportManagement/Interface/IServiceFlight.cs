3c9bfa23113cab7a2d79a116040e3dd73a8fc1e4
ksaay2000 <anoueramin.kassaa@esprit.tn>
2023-02-07 10:13:16 +0100

    atelier2 v0

---
+++ AirportManagement/Interface/IServiceFlight.cs
@@ -0,0 +1,15 @@
+﻿using AirportManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportManagement.Interface
+{
+    public interface IServiceFlight
+    {
+       
+       
+    }
+}
